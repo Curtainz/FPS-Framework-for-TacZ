77ad3e57b2ea28205ebaf0edf9b61feca2be3b28
CurtzVanilla <zielene@outlook.com>
2026-09-03 23:23:45 +0800

    修复坐标与摔死问题

--- kubejs/server_scripts/fps_framework/map_manager.js
+++ kubejs/server_scripts/fps_framework/map_manager.js
@@ -3,21 +3,22 @@ global.FPS = global.FPS || {};
 global.FPS.MAPS = {
     test: {
         id: 'test',
-        name: 'FPS 靶场测试',
+        name: 'FPS Test Range',
         dimension: 'fps:test',
-        lobby: { x: 0, y: 80, z: 0 },
+        // 平坦地图总厚度为5格，地面站立Y应为5
+        lobby: { x: 0, y: 5, z: 0 },
         spawns: {
             red: [
-                { x: -20, y: 80, z: 0, yaw: 90 },
-                { x: -20, y: 80, z: 5, yaw: 90 },
-                { x: -20, y: 80, z: -5, yaw: 90 },
-                { x: -25, y: 80, z: 0, yaw: 90 }
+                { x: -20, y: 5, z: 0, yaw: 90 },
+                { x: -20, y: 5, z: 5, yaw: 90 },
+                { x: -20, y: 5, z: -5, yaw: 90 },
+                { x: -25, y: 5, z: 0, yaw: 90 }
             ],
             blue: [
-                { x: 20, y: 80, z: 0, yaw: -90 },
-                { x: 20, y: 80, z: 5, yaw: -90 },
-                { x: 20, y: 80, z: -5, yaw: -90 },
-                { x: 25, y: 80, z: 0, yaw: -90 }
+                { x: 20, y: 5, z: 0, yaw: -90 },
+                { x: 20, y: 5, z: 5, yaw: -90 },
+                { x: 20, y: 5, z: -5, yaw: -90 },
+                { x: 25, y: 5, z: 0, yaw: -90 }
             ]
         }
     }
@@ -39,8 +40,6 @@ global.FPS.teleportSpawn = function(player, team, server) {
     const game = global.FPS.game;
     const map = global.FPS.getMap(game.map);
     const points = map.spawns[team];
-    
-    // 随机选择该队伍的一个出生点
     const p = points[Math.floor(Math.random() * points.length)];
 
     server.runCommandSilent(
